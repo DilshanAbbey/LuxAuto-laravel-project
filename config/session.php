065e45d3577ea87955a97f85f70fdec05d8f4a4f
CyberGryffin <dilsupabbey1gotmashmail@gmail.com>
2025-08-13 04:53:32 +0530

    changes as at 13/08/2025 @4:53am

--- config/session.php
+++ config/session.php
@@ -73,7 +73,7 @@ return [
     |
     */
 
-    'connection' => env('SESSION_CONNECTION'),
+    'connection' => env('SESSION_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -212,6 +212,6 @@ return [
     |
     */
 
-    'partitioned' => env('SESSION_PARTITIONED_COOKIE', false),
+    //'partitioned' => env('SESSION_PARTITIONED_COOKIE', false),
 
 ];
